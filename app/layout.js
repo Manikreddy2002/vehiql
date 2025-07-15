f67a5e3b9a8147459ca5177ef7e722b4d43c86f3
Nandu <nandeeshwarreddy666@gmail.com>
2025-07-15 19:07:16 +0530

    test 2 commit

--- app/layout.js
+++ app/layout.js
@@ -1,8 +1,9 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
 import Header from "@/components/header";
+import { ClerkProvider } from "@clerk/nextjs";
 
-const inter = Inter({subsets: ["latin"]});
+const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Vehiql",
@@ -11,18 +12,20 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body
-        className={`${inter.className}`}
-      >
-        <Header/>
-        <main className="min-h-screen">{children}</main>
-        <footer className="bg-blue-50 py-12">
-          <div className="container text-center text-gray-600 mx-auto px-4">
-            <p>Made by Team Kanyaraasi</p>
-          </div>
-        </footer>
-      </body>
-    </html>
+    <ClerkProvider>
+      <html lang="en">
+        <body
+          className={`${inter.className}`}
+        >
+          <Header />
+          <main className="min-h-screen">{children}</main>
+          <footer className="bg-blue-50 py-12">
+            <div className="container text-center text-gray-600 mx-auto px-4">
+              <p>Made by Team Kanyaraasi</p>
+            </div>
+          </footer>
+        </body>
+      </html>
+    </ClerkProvider>
   );
 }
