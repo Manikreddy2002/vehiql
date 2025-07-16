82a037f90ea261f16cbd75b53ef686580fb09e23
Nandu <nandeeshwarreddy666@gmail.com>
2025-07-16 08:07:55 +0530

    test 2 commit

--- components/header.jsx
+++ components/header.jsx
@@ -8,7 +8,7 @@ import { CarFront } from "lucide-react";
 const Header = async ({ isAdminPage = false }) => {
     const isAdmin = false;
     return (<header className="fixed top-0 w-full  bg-white/80 backdrop-blur-md z-50  border-b ">
-        <nav className="mx-auto px-4 py-4 flex items-center justify-">
+        <nav className="mx-auto px-4 py-4 flex items-center justify-between">
             <Link href={isAdminPage ? "/admin" : "/"}>
                 <Image src="/logo.png" alt="Vehiql Logo" width={200} height={60} className="h-12 w-auto object-contain" />
                 {isAdminPage && (
