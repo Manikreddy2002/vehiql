7d851e8423972ec419cb266ccb9ed14e520ace02
Manikreddy2002 <150599550+Manikreddy2002@users.noreply.github.com>
2025-08-07 22:52:56 +0530

    In admin page only Testdrive management  created.

---
+++ app/(admin)/admin/test-drives/page.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+export const metadata = {
+    title : "Test Drive | Vehiql",
+    description: "Manage Test Drive Bookings"
+};
+
+const TestDrivePage = () => {
+  return (
+    <div className='p-6'>
+        <h1 className='text-2xl font-bold mb-6 '> Test Drive Management</h1>
+        
+    </div>
+  )
+}
+
+export default TestDrivePage
