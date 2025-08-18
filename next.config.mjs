0f5ef04988148c66f156b382f5f83b90b2982a15
Nandu <nandeeshwarreddy666@gmail.com>
2025-08-18 17:20:02 +0530

    Deployment Bug Fixes

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
     experimental: {
+        optimizeCss: false,
         serverComponentsHmrCache: false,
         serverActions : {
             bodySizeLimit : '20mb'
